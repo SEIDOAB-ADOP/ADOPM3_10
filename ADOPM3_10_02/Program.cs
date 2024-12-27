3bdd1e7ec7831c4c5a10518b35d7bee3e2cfbfe3
Martin <martin_lenart@icloud.com>
2024-12-27 16:02:25 +0100

    update

--- ADOPM3_10_02/Program.cs
+++ ADOPM3_10_02/Program.cs
@@ -8,15 +8,19 @@ namespace ADOPM3_10_02
     {
         static void Main(string[] args)
         {
-            //Explcicitly set the key and iv
-            byte[] key = new byte[16]; // { 145, 12, 32, 245, 98, 132, 98, 214, 6, 77, 131, 44, 221, 3, 9, 50 };
-            byte[] iv = new byte[16];  // { 15, 122, 132, 5, 93, 198, 44, 31, 9, 39, 241, 49, 250, 188, 80, 7 };
-
+            //Set the key and iv
+            byte[] key = new byte[16]; 
+            byte[] iv = new byte[16];  
+            
             //If you randomly generate key and iv, they need to be stored in a file or you cannot decrypt
             //Key is stored in a secrect Key-repository, while the iv can be shared openly before encrypt/decrypt
             RandomNumberGenerator rand = RandomNumberGenerator.Create();
-            //rand.GetBytes(key);
-            //rand.GetBytes(iv);
+            rand.GetBytes(key);
+            rand.GetBytes(iv);
+
+            //Alternatively set key and iv explicitly
+            //key = new byte[] { 145, 12, 32, 245, 98, 132, 98, 214, 6, 77, 131, 44, 221, 3, 9, 50 };
+            //iv = new byte[] { 15, 122, 132, 5, 93, 198, 44, 31, 9, 39, 241, 49, 250, 188, 80, 7 };
 
             var stringToEncrypt = "The quick brown fox jumps over the lazy dog.";
 
@@ -54,6 +58,7 @@ namespace ADOPM3_10_02
             Console.WriteLine($"As byte[]:");
             foreach (byte b in decryptedBytes) Console.Write($"{b:x2} ");
             Console.WriteLine();
+
 
             static string fname(string name)
             {
--- ADOPM3_10_02b/Program.cs
+++ ADOPM3_10_02b/Program.cs
@@ -8,16 +8,20 @@ namespace ADOPM3_10_02b
     {
         static void Main(string[] args)
         {
-            //Explcicitly set the key and iv
-            byte[] key = new byte[16]; // { 145, 12, 32, 245, 98, 132, 98, 214, 6, 77, 131, 44, 221, 3, 9, 50 };
-            byte[] iv = new byte[16];  // { 15, 122, 132, 5, 93, 198, 44, 31, 9, 39, 241, 49, 250, 188, 80, 7 };
-
+            //Set the key and iv
+            byte[] key = new byte[16]; 
+            byte[] iv = new byte[16];  
+            
             //If you randomly generate key and iv, they need to be stored in a file or you cannot decrypt
             //Key is stored in a secrect Key-repository, while the iv can be shared openly before encrypt/decrypt
             RandomNumberGenerator rand = RandomNumberGenerator.Create();
-            //rand.GetBytes(key);
-            //rand.GetBytes(iv);
+            rand.GetBytes(key);
+            rand.GetBytes(iv);
 
+            //Alternatively set key and iv explicitly
+            //key = new byte[] { 145, 12, 32, 245, 98, 132, 98, 214, 6, 77, 131, 44, 221, 3, 9, 50 };
+            //iv = new byte[] { 15, 122, 132, 5, 93, 198, 44, 31, 9, 39, 241, 49, 250, 188, 80, 7 };
+            
             var stringToEncrypt = "The quick brown fox jumps over the lazy dog.";
 
             // This is clear text.
@@ -38,11 +42,10 @@ namespace ADOPM3_10_02b
             //encrypted message
             Console.WriteLine("\n\nThis is AES encryption:");
             string encryptedString = Convert.ToBase64String(encryptedBytes);
-            Console.WriteLine($"As string: {encryptedString}");
+            Console.WriteLine($"As Base64String: {encryptedString}");
             Console.WriteLine($"As byte[]:");
             foreach (byte b in encryptedBytes) Console.Write($"{b:x2} ");
             Console.WriteLine();
-
 
             //Decrypt using AES
             byte[] decryptedBytes;
@@ -59,6 +62,7 @@ namespace ADOPM3_10_02b
             Console.WriteLine($"As byte[]:");
             foreach (byte b in decryptedBytes) Console.Write($"{b:x2} ");
             Console.WriteLine();
+
 
             static string fname(string name)
             {
